053816694d8e1006f9f3fdea95299be2cc911c2c
Peter Langlois <peter.langlois@snapsheet.me>
2025-04-04 10:03:34 -0500

    - add ability to set a working directory - add support for pre-release versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 GITHUB_TOKEN=$1
 OWNER=$2
+WORKING_DIR=$3
 
 if [[ -z "${GITHUB_TOKEN}" ]]; then
   echo "Github token not provided";
@@ -11,11 +12,23 @@ if [[ -z "${OWNER}" ]]; then
   exit 2;
 fi
 
+if [[ -n "${WORKING_DIR}" ]]; then
+  echo "Changing to directory: ${WORKING_DIR}"
+  if ! cd "./${WORKING_DIR}"; then
+    echo "Failed to change to directory: ${WORKING_DIR}"
+    exit 1
+  fi
+fi
+
 git config --global --add safe.directory /github/workspace
 
 echo "Verifying the version matches the gem version"
 VERSION_TAG=$(echo $GITHUB_REF | cut -d / -f 3)
 GEM_VERSION=$(ruby -e "require 'rubygems'; gemspec = Dir.entries('.').find { |file| file =~ /.*\.gemspec/ }; spec = Gem::Specification::load(gemspec); puts spec.version")
+# pre release versions have a .pre. prefix after the minor version
+# '1.2.3.pre.beta.4' -> '1.2.3-beta.4'
+GEM_VERSION=${GEM_VERSION/.pre./-}
+
 if [[ $VERSION_TAG != $GEM_VERSION ]]; then
   echo "Version tag does not match gem version"
   exit 2;
